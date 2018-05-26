410083a0022e86782b9b666419393f6b4330cfb7
simeonch7 <simeonch7@gmail.com>
2018-05-26 21:53:20 +0300

    Adding some features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 
 Rails.application.routes.draw do
-  resources :faqs
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'

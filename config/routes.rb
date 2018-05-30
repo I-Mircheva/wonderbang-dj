4acc68634b3097b5d9bf845b8b80c84bbe88d295
Ivona Mircheva <ivona1mircheva@gmail.com>
2018-05-30 05:05:01 +0300

    Fix db and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 
 Rails.application.routes.draw do
 
+  resources :songs
+  resources :playlists
+  
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415203939) do
+ActiveRecord::Schema.define(version: 20180529192656) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "abouts", force: :cascade do |t|
-    t.text "content"
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
-  create_table "contact_us", force: :cascade do |t|
+  create_table "songs", force: :cascade do |t|
+    t.integer "provider"
+    t.string "uuid"
+    t.integer "playlist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "faqs", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "how_to_uses", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
+    t.index ["provider"], name: "index_songs_on_provider"
   end
 
   create_table "users", force: :cascade do |t|
